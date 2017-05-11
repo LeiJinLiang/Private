68340bacb6b71ec3c3c58a65726b3268835fad14
jinlei <jinleilei@shhxzq.com>
2017-05-11 18:41:23 +0800

    add App

---
+++ app/App.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react'
+import styles from './App.css';
+
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {test: 'foo'};
+    }
+    render() {
+        return(
+            <div className={styles.app}>
+                bar ....
+            </div>
+        )
+    }
+}
+
+export default App
